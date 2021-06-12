1b30cf2d8b50e072ad03d8cecf22ff01f8131861
itovero <itovero0829@gmail.com>
2021-06-12 16:49:27 -0700

    Added comments to progress.py

--- NeutronPy Newest/progress.py
+++ NeutronPy Newest/progress.py
@@ -1,11 +1,10 @@
 import sys
-#from PyQt5.QtWidgets import QApplication, QWidget, QProgressBar
-#from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import * 
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtGui import * 
 from PyQt5.QtCore import *
 
+#Class for the loading window!
 class Progress(QWidget):
     def __init__(self):
         super().__init__()
@@ -21,13 +20,16 @@ class Progress(QWidget):
         self.show()
 
 
-    def setValue(self, n, runtime = 1):
+    def setValue(self, n, runtime = 1): #Updates the percentage loaded to be shown
         self.progressBar.setValue(n)
         if (n == 100):
             self.setWindowTitle('Complete!')
             #self.setWindowTitle(f'Complete! Finishing loading data in {toc - tic:0.4f} seconds')
 
-    def finishFits2Array(self, timer):
+
+
+    #Couple of helper functions to update states
+    def finishFits2Array(self, timer): 
         self.setWindowTitle(f'Finished loading all .fits file in {timer:0.4f} seconds!')
         self.progressBar.setFormat(f"Finished loading all .fits file in {timer:0.4f} seconds!")
         self.progressBar.setAlignment(Qt.AlignCenter)
@@ -41,6 +43,9 @@ class Progress(QWidget):
         self.setWindowTitle(f'Finished loading Image Cube in {timer:0.4f} seconds!')
         self.progressBar.setFormat(f"Finished loading Image Cube in {timer:0.4f} seconds!")
         self.progressBar.setAlignment(Qt.AlignCenter)
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
